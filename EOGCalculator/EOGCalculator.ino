3c1dff4650b9736330a2de40b7b209617ba31c19
Ahmet Kaymak <ahmet.kaymak@stu.fsm.edu.tr>
2018-05-07 15:10:13 +0300

    V.0
    Arduino code has added

---
+++ EOGCalculator/EOGCalculator.ino
@@ -0,0 +1,33 @@
+// LE - Left Electrode
+// RE - Right Electrode
+// UE - Upper Electrode
+// DE - Down Electrode
+
+const int analogPinRL = A0;
+const int analogPinRLReversed = A1;
+const int analogPinUD = A2;
+const int analogPinUDReversed = A1;
+
+int RL = 0;
+int RLReversed = 0;
+int RLMain, UDMain;
+int UD = 0;
+int UDReversed = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  RL = analogRead(analogPinRL);
+  RLReversed = analogRead(analogPinRLReversed);
+  UD = analogRead(analogPinUD);
+  UDReversed = analogRead(analogPinUDReversed);
+  
+  RLMain=RL-RLReversed;
+  UDMain=UD-UDReversed;
+  Serial.print(RLMain);
+  Serial.print("\n");
+  Serial.flush();
+  delay(2);
+}
